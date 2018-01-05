27ab033a285f545486bdd1ab3d11aff8b57bfea9
nona <nawel.seh@live.fr>
2018-01-05 12:27:20 +0100

    creation d'un tableau

--- controle.java
+++ controle.java
@@ -2,21 +2,23 @@
 class Controle {
 	public static void calcul (long gauche ,long droite)
 	{
-		gauche = gauche +droite;
+		gauche = gauche + droite;
 	}
+	public static void tab(long [] gauche,long [] droite)
+	 
 	
     public static void main (String [] args) {
 		System.out.println('HELLO WORLD');
 		
-	long x,y;
-	x == 45;
-	y == 18;
-	System.out.println("x =" + x);
-	System.out.println ("y =" + y);
-	calcul(x, y);
-	System.out.println("x =" + x);
-	System.out.println ("y =" + y);
-		
-       
+	long gauche,droite;
+	gauche == 45;
+	groite == 18;
+	System.out.println("gauche =" + gauche);
+	System.out.println ("droite =" + droite);
+	calcul(gauche, droite);
+	System.out.println("gauche =" + gauche);
+	System.out.println ("droite =" + droite);
+	
+	   
     }
 }
