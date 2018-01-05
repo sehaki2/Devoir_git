523772ed740ec53e26f880d67834d129bfd23923
nona <nawel.seh@live.fr>
2018-01-05 12:01:12 +0100

    nouvelle methode calcul

---
+++ controle.java
@@ -0,0 +1,15 @@
+
+class Controle {
+	public static void calcul (long gauche ,long droite)
+	{
+		gauche = gauche +droite;
+	}
+	
+    public static void main (String [] args) {
+		System.out.println('HELLO WORLD');
+		
+	
+		
+       
+    }
+}
